ff633a93e1dce10134936adfa753bac25c425f3b
Nick Burton <1992nburton@gmail.com>
2026-03-20 06:24:43 -0700

    Fix lint script to use eslint@8 and keep eslint.config.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }]
+  }
+};
